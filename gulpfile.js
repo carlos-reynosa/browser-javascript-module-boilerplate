ec945568941205abf6ff0a9ee2d83e7321704827
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2015-06-14 21:17:04 -0700

    Added task documentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,84 @@
+//Helps run the build tasks
 var gulp = require('gulp');
-var uglify= require('gulp-uglify');
-var config=require('./gulp-config.json');
-var rename= require('gulp-rename');
-var jsHint= require('gulp-jshint');
+var config = require('./gulp-config.json');
 
+//Minifies and optimizes the module
+var uglify = require('gulp-uglify');
+
+//Used for renaming output files
+var rename = require('gulp-rename');
+
+//Used for checking the module for errors
+var jsHint = require('gulp-jshint');
 
 //Directories
-var sourceDirectory=config.directories.source;
-var distDirectory= config.directories.min;
-var currentDirectory='.';
+var sourceDirectory = config.directories.source;
+var distDirectory = config.directories.min;
+var currentDirectory = '.';
 
-var karma=require('karma').server;
+//Server for running tests
+var karma = require('karma').server;
 
 //Source paths
-var sourceFileName=config.fileNames.source;
-
-var sourcePath=sourceDirectory+'/'+sourceFileName;
+var sourceFileName = config.fileNames.source;
+var sourcePath = sourceDirectory + '/' + sourceFileName;
 
 //Min paths
-var minFileName=config.fileNames.min;
+var minFileName = config.fileNames.min;
+var minDistPath = distDirectory + '/' + minFileName;
 
-var minDistPath=distDirectory+'/'+minFileName;
-
-gulp.task('test',function(done){
-
-    console.log('Running tests....');
-
+/**
+ * Reads in the test program configuration and runs all tests within the test directory.
+ * If the tests fails the entire task fails.
+ */
+gulp.task('test', ['lint'], function (done) {
     karma.start({
-        configFile:__dirname+'/test.karma.conf.js',
+        configFile: __dirname + '/test.karma.conf.js',
         singleRun: true
 
-    },function(exitCode){
+    }, function (exitCode) {
 
-        if( exitCode == 0){
-           done();
-        }else{
+        if (exitCode == 0) {
+            done();
+        } else {
 
             process.exit(exitCode);
         }
     });
 });
 
-gulp.task('minify',['lint'],function(){
-
-    console.log('\nReading file '+sourcePath);
-    console.log('Renaming file to '+minFileName);
-    console.log('Writing minified file to '+minDistPath+' and '+currentDirectory+'\n');
-
+/**
+ * Tries to minify and optimize the module code. Checks if the module has any errors in style or code
+ * before ouputing the minified version.
+ */
+gulp.task('minify', ['lint', 'test'], function () {
     return gulp.src(sourcePath)
         .pipe(gulp.dest('.'))
         .pipe(uglify())
         .pipe(rename(minFileName))
         .pipe(gulp.dest(distDirectory))
         .pipe(gulp.dest('.'));
-
 });
 
-gulp.task('lint',['test'],function(cb){
-
-    console.log('Linting '+sourcePath);
+/**
+ *Checks the module code for style and code errors.
+ */
+gulp.task('lint', function () {
 
     return gulp.src(sourcePath)
         .pipe(jsHint())
-        .pipe(jsHint.reporter('jshint-stylish',{verbose:true}))
+        .pipe(jsHint.reporter('jshint-stylish', {verbose: true}))
         .pipe(jsHint.reporter('fail'));
 
-});
-
-gulp.task('build',['test','minify','lint'],function(){
-
 
 });
 
-gulp.task('default',['build'],function(){
+gulp.task('build', ['test', 'minify', 'lint'], function () {
+
+
+});
+
+gulp.task('default', ['build'], function () {
 
 });
 
